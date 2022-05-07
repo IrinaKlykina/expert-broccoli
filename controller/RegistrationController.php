dafb008c4ef7dbde0c0d333a8b2e993154ec235a
Alina <amanturova@secureducation.ru>
2022-05-07 15:17:37 +0300

    Перенесли функции в юзер

--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -7,42 +7,6 @@ use model\Database;
 
 class RegistrationController
 {
-    public function isPasswordBig($number)
-    {
-        if ($number >= 6) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function isPasswordSmall($number)
-    {
-        if ($number < 6) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function isAgeBig($user)
-    {
-        if ($user->age >= 18) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function isAgeSmall($age)
-    {
-        if ($age < 18) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public function indexAction()
     {
         require_once 'model/User.php';
@@ -51,15 +15,13 @@ class RegistrationController
             include(__DIR__ . '/../view/reg_form.html');
         }
         $db = new \model\Database();
-
         $sth = $db->dbh->prepare('SELECT * from user');
         $sth->execute();
         $data = $sth->fetchAll();
 
         $user = new \model\User($_POST);
-        $number = strlen($user->password);
 
-        if ($this->isPasswordBig($number) && $this->isAgeBig($user->age) && $user->password == $user->confirmPassword) {
+        if ($user->isPasswordBig() && $user->isAgeBig() && $user->password == $user->confirmPassword) {
             $sql = 'INSERT INTO user (password, login, name, age, gender) VALUES (:password, :login, :name, :age, :gender)';
             $stmt = $db->dbh->prepare($sql);
             $result = $stmt->execute([
@@ -70,13 +32,13 @@ class RegistrationController
                 'gender' => $user->gender,
             ]);
         } else {
-            if (!$this->isPasswordSmall($number)) {
+            if (!$user->isPasswordSmall()) {
                 echo "Пароль должен быть минимум 6 символов";
             }
-            if (!$this->isPasswordSmall($number) || $user->password !== $user->confirmPassword) {
+            if (!$user->isPasswordSmall() || $user->password !== $user->confirmPassword) {
                 include(__DIR__ . '/../view/reg_form.html');
             }
-            if (!$this->isAgeSmall ($user->age)) {
+            if ($user->isAgeSmall()) {
                 echo "Сюда нельзя!";
             }
             if ($user->password !== $user->confirmPassword) {
--- model/User.php
+++ model/User.php
@@ -4,13 +4,13 @@ namespace model;
 
 class User
 {
-
     public $age;
     public $name;
     public $login;
     public $password;
     public $confirmPassword;
     public $gender;
+    public $passwordLength;
 
     public function __construct($post)
     {
@@ -20,5 +20,44 @@ class User
         $this->name = $post['name'];
         $this->login = $post['login'];
         $this->gender = $post['gender'];
+
+        $this->passwordLength = strlen($this->password);
     }
+
+    public function isPasswordBig()
+    {
+        if ($this->passwordLength >= 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function isPasswordSmall()
+    {
+        if ($this->passwordLength < 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function isAgeBig()
+    {
+        if ($this->age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function isAgeSmall()
+    {
+        if ($this->age < 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
