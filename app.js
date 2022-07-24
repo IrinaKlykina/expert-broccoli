d3952bc9e8998437f1f6b8ffd9c25ac10440d6cc
Alina <amanturova@secureducation.ru>
2022-07-24 18:11:52 +0300

    подключен файл app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-    function checkPassword() {
-        let password = document.getElementById("Password");
-       // let confirmPassword = document.getElementById("confirmPassword");
-        let passwordEntered = password.value;
+function checkPassword() {
+    let password = document.getElementById("Password");
+   // let confirmPassword = document.getElementById("confirmPassword");
+    let passwordEntered = password.value;
 
-        if (passwordEntered => 6) {
-            return true;
-      }
-        alert("Должно быть 6 символов или больше!");
-        return false;
-    }
+    if (passwordEntered => 6) {
+        return true;
+  }
+    alert("Должно быть 6 символов или больше!");
+    return false;
+}
+
+function temp() {
+    alert('qq');
+}
